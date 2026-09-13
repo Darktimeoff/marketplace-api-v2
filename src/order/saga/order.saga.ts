2429d28d904abe9e37062c97130bf5e55571f74c
Yevhenii Korolikhin <yevheniikorolihin@gmail.com>
2026-09-13 23:18:00 +0200

    feat: add base state machine

--- src/order/saga/order.saga.ts
+++ src/order/saga/order.saga.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { Order } from "../entity/order.entity.js";
 import { Transactional } from "@nestjs-cls/transactional";
 import { OrderRepository } from "../repository/order.repository.js";
@@ -12,16 +12,28 @@ export class OrderSaga {
     private readonly repository: OrderRepository,
     private readonly offers: ProductOfferService
   ) {
-    
+
   }
 
   @Transactional()
   async handleById(orderId: Order['id']) {
-    const order = await this.repository.findByIdOrFail(orderId);
+    let order = await this.repository.findByIdOrFail(orderId);
+    let hasTransitioned = true;
 
+    while (hasTransitioned) {
+      hasTransitioned = await this.processState(order);
+
+      if (hasTransitioned) {
+        order = await this.repository.findByIdOrFail(orderId);
+      }
+    }
+  }
+
+  private async processState(order: Order): Promise<boolean> {
     switch (order.status) {
       case OrderStatusEnum.created:
-        await this.handlerCreatedStatus(order)
+        return await this.handlerCreatedStatus(order);
+
       case OrderStatusEnum.pending_payment:
       case OrderStatusEnum.failed_payment:
       case OrderStatusEnum.paid:
@@ -32,16 +44,18 @@ export class OrderSaga {
       case OrderStatusEnum.completed:
       case OrderStatusEnum.canceled:
       case OrderStatusEnum.refunded:
+        return false;
     }
   }
 
-  private async handlerCreatedStatus(order: Order) {
+  private async handlerCreatedStatus(order: Order): Promise<boolean> {
     const available = await this.reserveItemsInSeller(order)
     if (!available) {
-      return
+      return false
     }
 
     await this.repository.updateStatusById(order.id, OrderStatusEnum.pending_payment)
+    return true
   }
 
   private async reserveItemsInSeller(order: Order) {

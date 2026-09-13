6ec2de8df1918d0f8823a965fbdb2f4036bb8948
Yevhenii Korolikhin <yevheniikorolihin@gmail.com>
2026-09-13 23:51:21 +0200

    feat: add worker size

--- src/background-job/service/background-job.service.ts
+++ src/background-job/service/background-job.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { BackgroundJobRepository } from '../repository/background-job.repository.js';
 import { BackgroundJobCreateInput } from '../input/background-job-create.input.js';
 import { BackgroundJob } from '../entity/background-job.entity.js';
+import { BackgroundJobTypeEnum } from '../../entities/enums.js';
 
 @Injectable()
 export class BackgroundJobService {
   constructor(private readonly backgroundJobRepository: BackgroundJobRepository) {}
 
-  /** Ставит задачу в очередь. Статус не принимаем снаружи: новая задача всегда
-   *  QUEUED (default колонки), дальше её двигает воркер. */
   create(input: BackgroundJobCreateInput): Promise<BackgroundJob> {
     return this.backgroundJobRepository.create({ ...input, orderId: input.orderId ?? null });
+  }
+
+  claimNext(type: BackgroundJobTypeEnum): Promise<BackgroundJob | null> {
+    return this.backgroundJobRepository.claimNext(type);
+  }
+
+  markReady(id: BackgroundJob['id']): Promise<void> {
+    return this.backgroundJobRepository.markReady(id);
+  }
+
+  markFailed(id: BackgroundJob['id'], errorMessage: string): Promise<void> {
+    return this.backgroundJobRepository.markFailed(id, errorMessage);
   }
 }
